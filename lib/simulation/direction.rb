d39b50a77b297c0c3f1540f3880a5957d920c829
Ranbir Singh <ransingh57@yahoo.com>
2017-05-25 14:44:07 +1000

    moved all direction related stuff to Direction module

--- lib/simulation/direction.rb
+++ lib/simulation/direction.rb
@@ -1,34 +1,37 @@
 module Simulation
   module Direction
     NORTH = "NORTH"
-    SOUTH = "SOUTH"
+    NORTH_EAST = "NORTH_EAST"
     EAST  = "EAST"
+    SOUTH_EAST = "SOUTH_EAST"
+    SOUTH = "SOUTH"
+    SOUTH_WEST = "SOUTH_WEST"
     WEST  = "WEST"
-    
+    NORTH_WEST = "NORTH_WEST"
+
+    DIRECTIONS = [
+      NORTH,
+      NORTH_EAST,
+      EAST,
+      SOUTH_EAST,
+      SOUTH,
+      SOUTH_WEST,
+      WEST,
+      NORTH_WEST
+    ]
+
     def rotate_left_from(direction)
-      case direction
-      when NORTH
-        WEST
-      when WEST
-        SOUTH
-      when SOUTH
-        EAST
-      when EAST
-        NORTH
-      end
+      index_current_direction = DIRECTIONS.index(direction)
+      DIRECTIONS.rotate(index_current_direction).rotate(-1).first
     end
 
     def rotate_right_from(direction)
-      case direction
-      when NORTH
-        EAST
-      when EAST
-        SOUTH
-      when SOUTH
-        WEST
-      when WEST
-        NORTH
-      end
+      index_current_direction = DIRECTIONS.index(direction)
+      DIRECTIONS.rotate(index_current_direction).rotate.first
+    end
+
+    def direction_valid? direction
+      DIRECTIONS.include? direction
     end
   end
 end
